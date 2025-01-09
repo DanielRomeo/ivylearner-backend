d5411a4d80aba69556befc8e7cc3b91f73bbdeb9
danielromeo <danielromeo99@gmail.com>
2025-01-09 12:59:52 +0200

    aadding authentication,with passport. managed to get the user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { StudentsModule } from './students/students.module';
 import { StudentsController } from './students/students.controller';
 import { StudentsService } from './students/students.service';
 import { DatabaseModule } from './database/database.module';
+// import { InstructorsModule } from './instructors/instructors.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [StudentsModule, DatabaseModule],
+  imports: [StudentsModule, DatabaseModule, UsersModule, AuthModule],
   controllers: [AppController, StudentsController],
   providers: [AppService, StudentsService],
 })
