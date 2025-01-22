346f4499485962568770d17256930f65df3f07d1
danielromeo <danielromeo99@gmail.com>
2025-01-22 09:19:24 +0200

    fixed all import issues associated with the lessons

--- src/lessons/lessons.controller.ts
+++ src/lessons/lessons.controller.ts
@@ -15,15 +15,25 @@ import {
   InternalServerErrorException,
   HttpException,
 } from '@nestjs/common';
-import { AuthService } from 'src/auth/auth.service';
-import { CoursesService, CreateCourseDto } from './courses.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-
+import { CoursesService } from '../courses/courses.service';
+import { LessonsService } from './lessons.service';
 
 @Controller('lessons')
 @UseGuards(JwtAuthGuard)
 export class LessonsController {
     constructor(
         private readonly coursesService: CoursesService,
+        private readonly lessonsService: LessonsService,
     ){}
+
+    // get one lesson:
+
+    // get all lessons in a course:
+
+    // delete one lessons:
+
+    // create a lesson:
+
+    
 }
--- src/lessons/lessons.module.ts
+++ src/lessons/lessons.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { LessonsController } from './lessons.controller';
 import { LessonsService } from './lessons.service';
-import { DatabaseModule } from 'src/database/database.module';
+import {DatabaseModule} from '../database/database.module'
 import {CoursesModule} from '../courses/courses.module'
-// import { InstructorsModule } from 'src/instructors/instructors.module';
-// import { OrganisationsModule } from 'src/organisations/organisations.module';
 
 @Module({
   imports: [DatabaseModule, CoursesModule],
